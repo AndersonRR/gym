01b8ce458c45a6e476dc913fdd4837df1ac2936a
AndersonRR <anderson.ribeiro.r@outlook.com>
2023-08-30 14:42:05 -0300

    feat: finaliza layout tela Home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,71 @@
-import { VStack } from 'native-base';
+import { useState } from 'react';
+import { HStack, VStack, FlatList, Heading, Text } from 'native-base';
 
+import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
+import { ExerciseCard } from '@components/ExerciseCard';
 
 export function Home() {
+  const [groups, setGroups] = useState(['costas', 'ombros', 'pernas', 'peitoral', 'panturrilha']);
+  const [exercises, setExercises] = useState(['1', '2', '3', '4', '5']);
+  const [groupSelected, setGroupSelected] = useState(groups[0]);
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={groups}
+        keyExtractor={item => item}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+        renderItem={({ item }) => {
+          return (
+            <Group
+              name={item}
+              isActive={groupSelected === item}
+              onPress={() => setGroupSelected(item)}
+              key={item}
+            />
+          );
+        }}
+      ></FlatList>
+
+      <VStack
+        flex={1}
+        px={8}
+      >
+        <HStack
+          justifyContent="space-between"
+          mb={5}
+        >
+          <Heading
+            color="gray.200"
+            fontSize="md"
+          >
+            Exercícios
+          </Heading>
+
+          <Text
+            color="gray.200"
+            fontSize="sm"
+          >
+            4
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={item => item}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+          renderItem={({ item }) => {
+            return <ExerciseCard />;
+          }}
+        />
+      </VStack>
     </VStack>
   );
 }
