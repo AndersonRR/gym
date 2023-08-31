1f20f45a12c914c5a9947fc8fbb1c1edb90c5887
AndersonRR <anderson.ribeiro.r@outlook.com>
2023-08-31 15:07:07 -0300

    feat: adiciona seleção de fotos a partir da galeria

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,15 +1,47 @@
 import { useState } from 'react';
+import { Alert, TouchableOpacity } from 'react-native';
 import { Center, Heading, ScrollView, Skeleton, Text, VStack } from 'native-base';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
-import { ScreenHeader } from '@components/ScreenHeader';
-import { UserPhoto } from '@components/UserPhoto';
-import { TouchableOpacity } from 'react-native';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { UserPhoto } from '@components/UserPhoto';
+import { ScreenHeader } from '@components/ScreenHeader';
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://github.com/andersonrr.png');
   const PHOTO_SIZE = 33;
+
+  async function handleSelectPhoto() {
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
+
+        if (photoInfo.exists && photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          return Alert.alert('Imagem muito grande. Tamanho máximo é 5 MB.');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -29,13 +61,13 @@ export function Profile() {
             />
           ) : (
             <UserPhoto
-              source={{ uri: 'https://github.com/andersonrr.png' }}
+              source={{ uri: userPhoto }}
               alt="Foto do usuário"
               size={33}
             />
           )}
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleSelectPhoto}>
             <Text
               color="green.500"
               fontWeight="bold"
